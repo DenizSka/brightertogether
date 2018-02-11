357affba1a5fc98979f35e2ba170c4c8b52497e3
Deniz Skantz <denizskantz@hotmail.com>
2018-02-11 22:09:18 +0300

    org

--- src/components/elements/Footer.js
+++ src/components/elements/Footer.js
@@ -8,7 +8,7 @@ export const Footer = () => {
         <div className="nav">
           <ul className='cf'>
             <li className='cf'>
-                © BTNY 2018 | <a className= "cf" href="mailto:info@brightertogether.com">Contact us</a>
+                © BTNY 2018 | <a className= "cf" href="mailto:info@brightertogether.org">Contact us</a>
             </li>
           </ul>
         </div>
