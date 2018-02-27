86f3f20adec2b2c0c1fe52da5df743a0f6a30ca3
Deniz Skantz <denizskantz@hotmail.com>
2018-02-27 16:35:06 -0500

    index

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Brighter Together NY - Website
+
+  "scripts": {
+    "start": "react-scripts start",
+    "build": "react-scripts build",
+    "test": "react-scripts test --env=jsdom",
+    "eject": "react-scripts eject",
+    // "predeploy": "npm run build",
+    // "deploy": "gh-pages -d build"
+    
+  "homepage": "https://denizska.github.io/brightertogetherny.github.org/"
--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
-
 import { NavBar, Header, Footer } from "./components/elements";
 import { Home, WhatDo, About, NotFound, Mission } from "./components/pages";
 import "./App.css";
@@ -17,7 +16,6 @@ class App extends Component {
             <Route path="/whatwedo" component={WhatDo} />
             <Route path="/about" component={About} />
             <Route path="/mission" component={Mission} />
-            <Route path="*" component={NotFound} />
           </Switch>
         </main>
       </div>
